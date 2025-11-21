983d1e7cd2bd2c4af0c7501041155d2fe213ab6e
wakato <itlogin0606@gmail.com>
2025-11-21 18:45:21 +0900

    ざったいまーじ

--- front/js/welcome.js
+++ front/js/welcome.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const okButton = document.getElementById('welcome-ok-btn');
 
     okButton.addEventListener('click', () => {
-        // ★ 「OK」が押されたら /planet (index.html) に移動します
+        // ★ 「OK」が押されたら /planet (inaadex.html) に移動します
         window.location.href = '/planet';
     });
 });
