e16647b32fad55963bb25cb89c50d9f53ffcc424
wakato <itlogin0606@gmail.com>
2026-01-23 11:50:14 +0900

    そもそも惑星カード表示されないのでそこのエラーをしゅうせい/web上の話ね

--- front/js/card.js
+++ front/js/card.js
@@ -1,6 +1,7 @@
 // front/js/card.js
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+// ★修正: importmap を使用するように変更
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const params = new URLSearchParams(window.location.search);
 const username = params.get('username') || 'NITROYUKKURI';
