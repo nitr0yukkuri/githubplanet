170d77c9212f662a4b75380b1162c6fa0353b15d
わかと <itlogin0606@gmail.com>
2026-07-29 17:30:34 +0900

    fix: remove ineffective service worker fetch handler

--- sw.js
+++ sw.js
@@ -16,10 +16,3 @@ self.addEventListener('activate', event => {
   })());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
